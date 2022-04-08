0deaa7825167c59b1bf6edeb58dadee0d9c33ab6
soyashimada <rikua0023@yahoo.co.jp>
2022-04-08 23:48:02 +0900

    add: add migration file of board favorites and board setting

---
+++ database/migrations/2022_04_08_233641_add_board_setting_to_boards_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBoardSettingToBoardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('boards', function (Blueprint $table) {
+            $table->boolean('active_status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('boards', function (Blueprint $table) {
+            //アクティブ状況のカラムを削除
+            Schema::table('boards', function (Blueprint $table) {
+                $table->dropColumn(['active_status']);
+            });
+        });
+    }
+}
