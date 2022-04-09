b590bca590ffd80f42c9728c73faa6e3299fa38d
soyashimada <rikua0023@yahoo.co.jp>
2022-04-09 23:55:16 +0900

    update: update board favorites model

--- app/Models/BoardFavorite.php
+++ app/Models/BoardFavorite.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class BoardFavorite extends Model
 {
     use HasFactory;
+
+    protected $guarded = array('id');
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function posts(){
+        return $this->hasMany(Post::class);
+    }
 }
