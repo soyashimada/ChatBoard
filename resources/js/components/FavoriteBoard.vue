ea5267ab9c82cba68d13dac62aa7918b46f10eb0
soyashimada <rikua0023@yahoo.co.jp>
2022-04-28 00:00:44 +0900

    add: favoriteBoardView

---
+++ resources/js/components/FavoriteBoard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="favorite">
+        <i class="fa-regular fa-heart" :class="onClickFavorite"></i>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "FavoriteSomething",
+    props: {
+        favorite_num: Number,
+        board_id: Number,
+    },
+    data: function() {
+        return {
+            favorite_num: this.favorite_num,
+            isActive: false,
+        }
+    },
+    computed: {
+        calcFavoriteNum () {
+            //いいね数の更新処理
+        }
+    },
+    mounted: {
+        onClickFavorite (){
+            //クリックイベント
+        }
+    },
+    methods : {
+        putFavoriteData() {
+            axios.put(
+
+            ).then(
+                //お気に入り数のローカルプロパティ更新
+            ).catch(
+                
+            )
+        },
+        deleteFavoriteData() {
+            axios.delete(
+
+            ).then(
+                //お気に入り数のローカルプロパティ更新
+            ).catch(
+
+            )
+        }
+
+    }
+}
+
+</script>
